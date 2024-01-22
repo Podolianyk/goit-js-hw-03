0e35fb222d6114dcee51b69ad87f1ce78dc985b8
Podolianyk <147654168+Podolianyk@users.noreply.github.com>
2024-01-22 13:12:46 +0100

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,20 @@
+// функція приймає рядок (параметр message) та перевіряє його довжину
+//  відповідно до заданої максимальної довжини(параметр maxLength)
+function formatMessage(message, maxLength) {
+    // Якщо довжина рядка дорівнює або менша за maxLength, то функція повертає початковий рядок без змін
+    if (message.length <= maxLength) {
+    return message;
+}
+    else {
+        // Якщо довжина перевищує maxLength, то функція обрізає рядок до maxLength символів, 
+        // додає трикрапку "..." в кінці та повертає обрізану версію
+        return message.slice(0, maxLength) + "…";
+}
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
